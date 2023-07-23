38d5e1538e0ae84d54bc65d101821b513c605415
ikramarenko1 <il.kramarenko1@gmail.com>
2023-07-23 16:25:15 +0300

    add homeworks 14-15

--- lesson_7/homework15.py
+++ lesson_7/homework15.py
@@ -1,3 +1,13 @@
 # Завдання 15: Перевірити, чи є словник пустим
 # Опис: Напишіть програму, яка приймає словник (dictionary) і перевіряє, чи є він пустим
 # (не містить жодного елемента). Використайте цю програму, щоб перевірити, чи є словник пустим.
+
+def if_empty(dictionary):
+    return len(dictionary) == 0
+
+
+fruits = {"apple": "red", "banana": "yellow", "orange": "orange", "grapes": "purple"}
+empty_fruits = {}
+
+print(if_empty(fruits))  # False
+print(if_empty(empty_fruits))  # True
